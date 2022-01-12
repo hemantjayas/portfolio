a7792b3cfd4a1c2ff0d1bc75e17f6bfa8d70473d
Hemant Kumar Jayas <87421907+hemantjayas@users.noreply.github.com>
2022-01-12 22:53:29 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,14 +5,14 @@ export default function About() {
   return (
     <div id="about">
       <h1>About Me</h1>
-      <p>I love to create beautiful and efficient websites for my customers.</p>
+      <p>I love to create beautiful and efficient websites for users.</p>
       <p>
-        My field of Interest's are building new Web Technologies and Products
-        and also in areas related to Deep Learning and Natural Launguage
+        My field of Interests are building new Web Technologies, Products
+        and also in Areas Related to Deep Learning and Natural Launguage
         Processing.
       </p>
       <p>
-        Whenever possible, I also apply my passion for developing products with
+        I apply my passion for developing products with
         Node.js and Modern Javascript Library and Frameworks like React.js and
         Next.js
       </p>
